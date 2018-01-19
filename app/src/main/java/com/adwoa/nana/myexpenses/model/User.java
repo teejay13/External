515d9f3e9cb2024ca31bc24deefddd81186fd187
teejay13 <adetunjisalako@gmail.com>
2018-01-19 20:37:21 +0000

    user functions

--- app/src/main/java/com/adwoa/nana/myexpenses/model/User.java
+++ app/src/main/java/com/adwoa/nana/myexpenses/model/User.java
@@ -1,23 +1,23 @@
 package com.adwoa.nana.myexpenses.model;
 
 public class User {
-    private int id;
+    private String id;
     private String name;
     private String email;
     private String password;
 
-    public User(int id, String name, String email, String password) {
+    public User(String id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
--- app/src/main/java/com/adwoa/nana/myexpenses/util/DatabaseHelper.java
+++ app/src/main/java/com/adwoa/nana/myexpenses/util/DatabaseHelper.java
@@ -1,26 +1,118 @@
 package com.adwoa.nana.myexpenses.util;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.adwoa.nana.myexpenses.model.User;
+
+import java.util.HashMap;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "my_expense";
+
+    private static final String TABLE_USER = "users";
+
+    // Login Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_UID = "uid";
+    private static final String KEY_CREATED_AT = "created_at";
+
+    public static final String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_USER + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+            + KEY_EMAIL + " TEXT UNIQUE," + KEY_UID + " TEXT,"
+            + KEY_CREATED_AT + " TEXT" + ")";
+
+
+
+    public DatabaseHelper(Context context, SQLiteDatabase.CursorFactory factory) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
-        super(context, name, factory, version, errorHandler);
+    public DatabaseHelper(Context context, SQLiteDatabase.CursorFactory factory, DatabaseErrorHandler errorHandler) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION, errorHandler);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+        sqLiteDatabase.execSQL(CREATE_LOGIN_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Drop older table if existed
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
 
+        // Create tables again
+        onCreate(sqLiteDatabase);
+    }
+
+    public void addUser(String name, String email, String uid, String created_at) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name); // Name
+        values.put(KEY_EMAIL, email); // Email
+        values.put(KEY_UID, uid); // Email
+        values.put(KEY_CREATED_AT, created_at); // Created At
+
+        // Inserting Row
+        long id = db.insert(TABLE_USER, null, values);
+        db.close(); // Closing database connection
+
+    }
+
+    public User Authenticate(User user){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USER,new String[]{KEY_ID,KEY_NAME,KEY_EMAIL,KEY_UID},
+                KEY_EMAIL + "=?", new String[]{user.getEmail()},//Where clause
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()&& cursor.getCount()>0) {
+            //if cursor has value then in user database there is user associated with this given email
+            User user1 = new User(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3));
+
+            //Match both passwords check they are same or not
+            if (user.getPassword().equalsIgnoreCase(user1.getPassword())) {
+                return user1;
+            }
+        }
+
+        //if user password does not matches or there is no record with that email then return @false
+        return null;
+    }
+
+
+    /**
+     * Getting user data from database
+     * */
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT  * FROM " + TABLE_USER;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // Move to first row
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            user.put("name", cursor.getString(1));
+            user.put("email", cursor.getString(2));
+            user.put("uid", cursor.getString(3));
+            user.put("created_at", cursor.getString(4));
+        }
+        cursor.close();
+        db.close();
+        // return user
+
+        return user;
     }
 }
